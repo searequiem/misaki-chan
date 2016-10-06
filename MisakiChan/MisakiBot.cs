ddc3faac60f1de282c8ff4e841f4a4a88591ee81
Vincent <Vincent@VINCENT-UB>
2016-10-06 15:24:20 -0700

    Add project files.

---
+++ MisakiChan/MisakiBot.cs
@@ -0,0 +1,46 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MisakiChan
+{
+    class MisakiBot
+    {
+        DiscordClient discord;
+        public MisakiBot()
+        {
+            discord = new DiscordClient(x =>
+            {
+                x.LogLevel = LogSeverity.Info;
+                x.LogHandler = Log;
+            });
+
+            discord.UsingCommands(x =>
+            {
+                x.PrefixChar = '>';
+                x.AllowMentionPrefix = true;
+            });
+
+            var commands = discord.GetService<CommandService>();
+
+            commands.CreateCommand("hello")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendMessage("whaddup, it's ya boi skinny penis");
+                });
+
+            discord.ExecuteAndWait(async () =>
+            {
+                await discord.Connect("MjMzNjgzMzk3NzU3NjMyNTE0.CthJ-g.qX-nRXra19WfVgfg2O4HG0GSXTM", TokenType.Bot);
+            });
+        }
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
---
+++ MisakiChan/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MisakiChan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MisakiBot bot = new MisakiBot();
+        }
+    }
+}
