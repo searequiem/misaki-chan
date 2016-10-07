b4d3937a5eaaf10cdf2f613501d82ad21e6eaf73
Vincent <searequiemjp@gmail.com>
2016-10-06 19:04:20 -0700

    Added ">pkmn" command.

--- MisakiChan/MisakiBot.cs
+++ MisakiChan/MisakiBot.cs
@@ -2,6 +2,7 @@
 using Discord.Commands;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +12,13 @@ namespace MisakiChan
     class MisakiBot
     {
         DiscordClient discord;
+        CommandService commands;
+        Random rand;
+        String[] randomPkmnFromText;
+
         public MisakiBot()
         {
+            
             discord = new DiscordClient(x =>
             {
                 x.LogLevel = LogSeverity.Info;
@@ -25,19 +31,29 @@ namespace MisakiChan
                 x.AllowMentionPrefix = true;
             });
 
-            var commands = discord.GetService<CommandService>();
+            commands = discord.GetService<CommandService>();
 
-            commands.CreateCommand("hello")
-                .Do(async (e) =>
-                {
-                    await e.Channel.SendMessage("whaddup, it's ya boi skinny penis");
-                });
+            randomPokemon();
 
             discord.ExecuteAndWait(async () =>
             {
-                await discord.Connect("MjMzNjgzMzk3NzU3NjMyNTE0.CthJ-g.qX-nRXra19WfVgfg2O4HG0GSXTM", TokenType.Bot);
+                await discord.Connect("", TokenType.Bot);
             });
         }
+
+        private void randomPokemon() 
+        {
+            commands.CreateCommand("pkmn")
+                .Do(async (e) =>
+                 {
+                     rand = new Random();
+                     randomPkmnFromText = File.ReadAllLines("pokemon.txt");
+                     string pokemonToPost = randomPkmnFromText[rand.Next(randomPkmnFromText.Length)];
+                     string bulbapediaLink = "http://bulbapedia.bulbagarden.net/wiki/" + pokemonToPost;
+                     await e.Channel.SendMessage(bulbapediaLink);
+                 });
+        }
+
         private void Log(object sender, LogMessageEventArgs e)
         {
             Console.WriteLine(e.Message);
